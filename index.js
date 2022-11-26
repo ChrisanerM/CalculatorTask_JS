019fa6eb26ad586f354a78b0424420f92d93bd26
ChrisanerM <chrisanermatthys04@gmail.com>
2022-11-26 12:13:52 +0200

    javascript

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-  
+let output = document.getElementById("dsply-box")
+let  myDecimal = true;
+
+function calculation(anyVal) {
+    output.value = output.value + anyVal;
+}
+function operators(anyVal) {
+    output.value = output.value + anyVal;
+     myDecimal = true; 
+}
+function del() {
+    output.value = output.value.slice(0, -1);
+}
+function decimal(anyVal) {
+  if (myDecimal === true) {
+      output.value = output.value + anyVal;
+      myDecimal = false; 
+  }
+
+}
+function clr() {
+    output.value = "";
+    output.value = output.value + anyVal;
+     myDecimal = true; 
+}
+function equal() {
+    try {
+        output.value = eval(output.value)
+    }
+    catch (err) {
+        output.value = "Error!"
+     }
+}
