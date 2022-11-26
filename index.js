acaa6f7b892a3ea84798961b757b51d10478fe85
ChrisanerM <chrisanermatthys04@gmail.com>
2022-11-26 12:59:11 +0200

    last commit

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 let output = document.getElementById("dsply-box")
 let  myDecimal = true;
 
-function calculation(anyVal) {
-    output.value = output.value + anyVal;
+function calculation(anyInput) {
+    output.value = output.value + anyInput;
 }
-function operators(anyVal) {
-    output.value = output.value + anyVal;
+function operators(anyInput) {
+    output.value = output.value + anyInput;
      myDecimal = true; 
 }
-function del() {
+function del () {
     output.value = output.value.slice(0, -1);
 }
-function decimal(anyVal) {
+function decimal(anyInput) {
   if (myDecimal === true) {
-      output.value = output.value + anyVal;
+      output.value = output.value + anyInput;
       myDecimal = false; 
   }
 
 }
 function clr() {
     output.value = "";
-    output.value = output.value + anyVal;
+    output.value = output.value + anyInput;
      myDecimal = true; 
 }
 function equal() {
